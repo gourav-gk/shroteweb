81471a47e102f7a1adce876307e38141f477c6ba
Gourav Kumar <gourav.20cse306@citranchi.ac.in>
2023-08-28 19:03:49 +0530

    modified content

--- link.php
+++ link.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <!-- Favicons -->
 <link href="./assets/img/about/Shrote-Water-Logo.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
@@ -15,22 +14,4 @@
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-=======
-<!-- Favicons -->
-<link href="./assets/img/about/Shrote-Water-Logo.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
->>>>>>> Stashed changes
   <link href="assets/css/style.css" rel="stylesheet">
